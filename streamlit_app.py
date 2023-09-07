7b62533ad5edf45c7113923605156ba1e189dcb0
Tagless <69176518+TaglessOfficial@users.noreply.github.com>
2023-09-07 00:01:14 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,35 +4,29 @@ import math
 import pandas as pd
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+def main():
+    st.title("Gorilla LLM")
+    input_prompt = st.text_area("Enter your prompt:")
+    
+    model_options = ('gorilla-7b-hf-v1', 'gorilla-mpt-7b-hf-v0')
+    option = st.selectbox('Select a model:', model_options)
+    
+    if st.button("Create"):
+        if len(input_prompt) > 0:
+            result = get_gorilla_response(prompt=input_prompt, model=option)
+            st.write(result)
+            # ...
+if st.button("Gorilla Magic"):
+    if len(input_prompt) > 0:
+        # ...
+        with col2:
+            code_result = extract_code_from_output(result)
+            if option == "gorilla-7b-hf-v1":
+                st.subheader("Generated Output")
+                st.code(code_result, language='python')
+            elif option == "gorilla-mpt-7b-hf-v0":
+                lines = code_result.split('\\n')
+                for line in lines[:-1]:
+                    st.code(line, language='python')
+            # ...
+            run_generated_code(file_path)
